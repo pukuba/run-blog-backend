e672c9509e5d1750347295a64fce524329bd1686
SeungWon <20sunrin041@sunrint.hs.kr>
2021-03-02 06:50:54 +0900

    Update Type

--- src/resolvers/Types.ts
+++ src/resolvers/Types.ts
@@ -1,8 +1,12 @@
 import { Db } from "mongodb"
-import { Loaders, Post } from "config/types"
+import { Loaders, Post, Comment } from "config/types"
 
 export default {
     Post: {
-        comments: (parent: Post, args: void, { db, loaders }: { db: Db, loaders: Loaders }) => loaders.commentsLoader.load(parent._id)
+        id: (parent: Post) => parent._id,
+        comments: (parent: Post, args: void, { loaders }: { loaders: Loaders }) => loaders.commentsLoader.load(parent._id)
+    },
+    Comment: {
+        id: (parent: Comment) => parent._id
     }
 }
